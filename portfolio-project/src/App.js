a1f8cdebc0ea65edf32bce80f28079916d236b97
shailjadosar12 <shailjadosar12@gmail.com>
2021-08-17 13:20:45 +0530

    navbar commit

---
+++ portfolio-project/src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navbar";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
