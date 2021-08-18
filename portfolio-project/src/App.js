5b02880958be5504168a2544f26ac2f05575395e
shailjadosar12 <shailjadosar12@gmail.com>
2021-08-18 11:08:58 +0530

    media changes done

--- portfolio-project/src/App.js
+++ portfolio-project/src/App.js
@@ -2,23 +2,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Components/Navbar";
 import "./App.css";
 import Header from "./Components/Header";
-import Particles from 'react-particles-js';
+import Particles from "react-particles-js";
 function App() {
   return (
     <div>
-      <Particles params ={{
-        particles:{
-          number:{
-            value:30,
-            density:{
-              enable:true,
-              value_area:900
-            }
-          }
-        }
-      }} />
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+          },
+        }}
+      />
       <Navbar />
-      <Header/>
+      <Header />
     </div>
   );
 }
